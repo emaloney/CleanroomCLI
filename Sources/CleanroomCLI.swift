cbd62fa4c0ebf18e9018cb660701ac61765546c8
Evan Maloney <emaloney@gilt.com>
2016-10-12 17:42:41 -0400

    Initial commit of CleanroomCLI
    Automated by createFrameworkRepo.sh

---
+++ Sources/CleanroomCLI.swift
@@ -0,0 +1,13 @@
+//
+//  CleanroomCLI.swift
+//  CleanroomCLI
+//
+//  Created by Evan Maloney on 10/12/16.
+//  Copyright © 2016 Gilt Groupe. All rights reserved.
+//
+
+public protocol CleanroomCLI
+{
+    // this is a dummy protocol; it ensures that a
+    // Swift header is generated (needed to compile)
+}
